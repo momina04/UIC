63a6d1da5f7b10326340b8e4f72f6cc687662e88
Ritesh Agarwal(Omi) <riteshja88@gmail.com>
2013-04-07 10:39:01 -0500

    Linked list untested

---
+++ spring2013/cs474/cpp_hw1/linked_list.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  linked_list.cpp
+ *
+ *    Description:  Implementation of Linked List that stores generic object
+ *
+ *        Version:  1.0
+ *        Created:  04/07/2013 05:25:06 AM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Ritesh Agarwal (Omi), ragarw8@uic.edu
+ *        Company:  University of Illinois, Chicago
+ *
+ * =====================================================================================
+ */
+
+
---
+++ spring2013/cs474/cpp_hw1/linked_list.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  linked_list.h
+ *
+ *    Description:  Linked List that stores generic objects
+ *
+ *        Version:  1.0
+ *        Created:  04/07/2013 05:24:44 AM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Ritesh Agarwal (Omi), ragarw8@uic.edu
+ *        Company:  University of Illinois, Chicago
+ *
+ * =====================================================================================
+ */
+
+
