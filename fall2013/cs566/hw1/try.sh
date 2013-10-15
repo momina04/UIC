fe2ab7f3d97480b98609daf949278fc2e1d56ba4
Ritesh Agarwal(Omi) <riteshja88@gmail.com>
2013-10-15 12:23:34 -0500

    Added test results

--- fall2013/cs566/hw1/try.sh
+++ fall2013/cs566/hw1/try.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-for n in 32 64 128 256 512 1024 2048
-    do for k in 1 2 4
-       do for p in 1 2 4
-        do make run N=$n K=$k P=$p 
+make delete
+    for k in 8
+       do for p in 2 
+        do for n in 64 128 256 512 1024 2048
+        do make run N=$n K=$k P=$p
         done
     done
 done
